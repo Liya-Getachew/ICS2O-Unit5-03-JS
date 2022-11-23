212ba939856beda344bf4751d3970c6f4145a71b
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-11-23 18:50:00 +0000

    edit text and css

--- js/script.js
+++ js/script.js
@@ -25,15 +25,15 @@ function ageRating() {
   // process
   if (age >= 17) {
     document.getElementById("watchable").innerHTML =
-      "My watchable movies: You're old enough to watch any R movie alone. Congrats!"
+      "You're old enough to watch any R movie alone. Congrats!"
   } else if (age >= 13) {
     document.getElementById("watchable").innerHTML =
-      "My watchable movies: You can watch PG-13 movies alone."
+      "You can watch PG-13 movies alone."
   } else if (age >= 5) {
     document.getElementById("watchable").innerHTML =
-      "My watchable movies: You're pretty young... let's stick to G or PG for now."
+      "You're pretty young... let's stick to G or PG for now."
   } else {
     document.getElementById("watchable").innerHTML =
-      "I'm surprised you even found this site... go watch some cocomelon kid."
+      "I'm surprised you even found this site... shouldn't you be napping?"
   }
 }
